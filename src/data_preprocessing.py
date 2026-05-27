b90571d5714325afe3e57310f49e339cb447fd6d
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 11:45:10 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,28 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
+
+def encode_features(df):
+
+    encoder = LabelEncoder()
+
+    categorical_cols = ['matchType']
+
+    for col in categorical_cols:
+
+        if col in df.columns:
+
+            df[col] = encoder.fit_transform(
+                df[col]
+            )
+
+    return df
+
+
+def scale_features(X):
+
+    scaler = StandardScaler()
+
+    X_scaled = scaler.fit_transform(X)
+
+    return X_scaled
