6a5de5aba62ee72e043878dfb98dc16613cb2350
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 11:45:29 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,43 @@
+def create_features(df):
 
+    if (
+        'kills' in df.columns and
+        'walkDistance' in df.columns
+    ):
+
+        df[
+            'kill_efficiency'
+        ] = (
+
+            df['kills']
+
+            /
+
+            (
+                df['walkDistance']
+                + 1
+            )
+
+        )
+
+    if (
+        'damageDealt' in df.columns and
+        'kills' in df.columns
+    ):
+
+        df[
+            'damage_per_kill'
+        ] = (
+
+            df['damageDealt']
+
+            /
+
+            (
+                df['kills']
+                + 1
+            )
+
+        )
+
+    return df
