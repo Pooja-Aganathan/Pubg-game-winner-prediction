7cb403976fc50563ac41291c57236bc963be1621
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 11:46:32 +0530

    Update model_evaluation.py

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,1 +1,68 @@
+from sklearn.metrics import (
 
+    mean_absolute_error,
+
+    mean_squared_error,
+
+    r2_score
+
+)
+
+import numpy as np
+
+
+def evaluate_model(
+
+    model,
+
+    X_test,
+
+    y_test
+
+):
+
+    predictions = model.predict(
+
+        X_test
+
+    )
+
+    results = {
+
+        'MAE':
+
+        mean_absolute_error(
+
+            y_test,
+
+            predictions
+
+        ),
+
+        'RMSE':
+
+        np.sqrt(
+
+            mean_squared_error(
+
+                y_test,
+
+                predictions
+
+            )
+
+        ),
+
+        'R2':
+
+        r2_score(
+
+            y_test,
+
+            predictions
+
+        )
+
+    }
+
+    return results
